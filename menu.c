103d8e6217b4ff02bbe8e03e3aa39224d5199281
Ben Saxon <c3307209@uon.edu>
2019-04-26 04:04:15 +0000

    added commenting

--- menu.c
+++ menu.c
@@ -1,118 +1,122 @@
 #include<stdio.h>
  
- 
+ /*the following code will take rotation and subststution encrypted text from external files, then decipher according to 
+  *which option is chosen in the switch function. the key for the rotation cipher can be changed in the encryption selection 
+  */
 int main()
 {
-FILE *input1;
+FILE *input1; 
 FILE *input2;
 FILE *input3;
 FILE *input4;
 FILE *input5;
 FILE *input6;
-input1 = fopen("encrypted_message","r");
-input2 = fopen("encrypted_message2","r");
-input3 = fopen("message2encrypt","r");
-input4 = fopen("sub_encrypted_message1","r");
-input5 = fopen("sub_encrypted_message2","r");
-input6 = fopen("message2encrypt","r");
-int x,dkey,ekey;
-printf("please choose from the following options:\n 1.Decipher roatation cipher #1 (key preset)\n 2.Decipher roatation cipher #2 (key preset)\n 3.Decipher roatation cipher #2 (input key)\n 4.Encrypt using rotation cipher\n 5.Decipher substitution cipher #1 (key preset)\n 6.Decipher substitution cipher #2 (key preset)\n 7.Encrypt using substitution cipher (key preset)\n");
-scanf("%d",&x);
-char a,b,c,d,e,f,g;
+input1 = fopen("encrypted_message","r"); //assigns the first rotation cipher to input 1 
+input2 = fopen("encrypted_message2","r"); //assigns the second rotation cipher to input 2
+input3 = fopen("message2encrypt","r"); //assigns a custom script to be encrypted to input 3. this script is used for both rotaton and substitution cipher
+input4 = fopen("sub_encrypted_message1","r"); //assigns the first substitution cipher to input 4
+input5 = fopen("sub_encrypted_message2","r"); //assigns the second substitution cipher to input 5
+input6 = fopen("message2encrypt","r"); //assigns the same custom script from input 3 to input 6
+int x,dkey,ekey; // the integer x will be used in switch function, dkey for decryption key selection, ekey for encryption key selection
+printf("please choose from the following options:\n 1.Decipher roatation cipher #1 (key preset)\n 2.Decipher roatation cipher #2 (key preset)\n 3.Decipher roatation cipher #2 (input key)\n 4.Encrypt using rotation cipher\n 5.Decipher substitution cipher #1 (key preset)\n 6.Decipher substitution cipher #2 (key preset)\n 7.Encrypt using substitution cipher (key preset)\n"); //displaying choices for switch function
+scanf("%d",&x); // slection for switch function
+char a,b,c,d,e,f,g; // these char will be used for substitution and rotation algorithms
  
 switch (x)
 {
-    case 1:
+    case 1: //Runs Decipher roatation cipher #1 (key preset)
     
-    while (feof(input1)==0) 
+    printf("decrypted message: \n"); //for display purpose
+    while (feof(input1)==0) //while input file has characters to be read (==0 is TRUE) 
     {
-        fscanf(input1,"%c",&a);
-        if(a >= 'A' && a <= 'Z')
+        fscanf(input1,"%c",&a); // calls input 1 and %c will read a single character for each loop
+        if(a >= 'A' && a <= 'Z') // limits characters to be read to capital letters in ASCII 
         {
-            a = a + 19;
+            a = a + 19; // adds (key) = 19 characters in ASCII to each character from file 
             if(a > 'Z')
             {
-                a = a - 'Z' + 'A' - 1;
+                a = a - 'Z' + 'A' - 1; // when key is added and exceeds 'Z', the addition continues from 'A'
             }
         }
-        printf("%c", a);
+        printf("%c", a); // prints one decrypted character each loop
     }
-    printf("\n");
+    printf("\n"); // adds new line after decrypted message
     ;
-    break;
+    break; //finishes case 1
     
-    case 2:
+    case 2: //Runs Decipher roatation cipher #2 (key preset)
     
-    while (feof(input2)==0) 
+    printf("decrypted message: \n"); //for display purpose
+    while (feof(input2)==0) //while input file has characters to be read (==0 is TRUE)
     {
-        fscanf(input2,"%c",&b);
-        if(b >= 'A' && b <= 'Z')
+        fscanf(input2,"%c",&b); // calls input 2 and %c will read a single character for each loop
+        if(b >= 'A' && b <= 'Z') // limits characters to be read to capital letters in ASCII 
         {
-			b = b + 12;
+			b = b + 12; // adds (key) = 12 characters in ASCII to each character from file 
 			if(b > 'Z')
 			{
-				b = b - 'Z' + 'A' - 1;
+				b = b - 'Z' + 'A' - 1; // when key is added and exceeds 'Z', the addition continues from 'A'
             }
         }
-        printf("%c", b);
+        printf("%c", b); // prints one decrypted character each loop
     }
-    printf("\n");
+    printf("\n"); // adds new line after decrypted message
     ;
-    break;
+    break; //finishes case 2
     
-    case 3:
+    case 3: //Runs Decipher roatation cipher #2 (input key)
     
-    printf("enter decryption key:\n");
-    scanf("%d",&dkey);
-    printf("decrypted message: \n");
+    printf("enter decryption key:\n"); //prompts key input
+    scanf("%d",&dkey); //assigns key input to dkey
+    printf("decrypted message: \n"); //for display purpose
 	
-    while (feof(input2)==0) 
+    while (feof(input2)==0) //while input file has characters to be read (==0 is TRUE)
     {
-        fscanf(input2,"%c",&g);
-        if(g >= 'A' && g <= 'Z')
+        fscanf(input2,"%c",&g); // calls input 2 and %c will read a single character for each loop
+        if(g >= 'A' && g <= 'Z') // limits characters to be read to capital letters in ASCII 
         {
-			g = g + dkey;
+			g = g + dkey; //adds decryption key to characters from input 2
 			if(g > 'Z')
 			{
-				g = g - 'Z' + 'A' - 1;
+				g = g - 'Z' + 'A' - 1; // when key is added and exceeds 'Z', the addition continues from 'A'
             }
         }
-        printf("%c", g);
+        printf("%c", g); // prints one decrypted character each loop
     }
-    printf("\n");
+    printf("\n"); // adds new line after decrypted message
     ;
-    break;
+    break; //finishes case 3
     
-    case 4:
+    case 4: //Runs Encrypt using rotation cipher using key input
     
-    printf("enter encryption key:\n");
-    scanf("%d",&ekey);
-    printf("encrypted message: \n");
+    printf("enter encryption key:\n"); //prompt key input
+    scanf("%d",&ekey); //assigns key to ekey
+    printf("encrypted message: \n"); //for display purpose
 	
-    while (feof(input3)==0) 
+    while (feof(input3)==0) //while input file has characters to be read (==0 is TRUE)
     {
-        fscanf(input3,"%c",&c);
-        if(c >= 'A' && c <= 'Z')
+        fscanf(input3,"%c",&c); // calls input 3 and %c will read a single character for each loop
+        if(c >= 'A' && c <= 'Z') // limits characters to be read to capital letters in ASCII
         {
-			c = c + ekey;
+			c = c + ekey; //adds encryption key to characters from input 3
 			if(c > 'Z')
 			{
-				c = c - 'Z' + 'A' - 1;
+				c = c - 'Z' + 'A' - 1; // when key is added and exceeds 'Z', the addition continues from 'A'
             }
         }
-        printf("%c", c);
+        printf("%c", c); // prints one decrypted character each loop
     }
-    printf("\n");
+    printf("\n"); // adds new line after decrypted message
     ;
-    break;
+    break; //finishes case 4
     
-    case 5:
+    case 5: //Runs Decipher substitution cipher #1 (key preset)
     
-    while (feof(input4)==0)
+    while (feof(input4)==0) //while input file has characters to be read (==0 is TRUE)
     {
-        fscanf(input4,"%c",&d);
+        fscanf(input4,"%c",&d); // calls input 4 and %c will read a single character for each loop
      
-        if (d == 'A') 
+        if (d == 'A') //the following script will substitute characters from input 4 for corresponding decryption characters found externally
         {
             d = 'K';
         }
@@ -204,19 +208,19 @@ switch (x)
                                                                                                 {
                                                                                                     d = 'L';
                                                                                                 }
-    			                                                                                printf("%c", d);
+    			                                                                                printf("%c", d); // prints one decrypted character each loop
     }
-    printf("\n");
+    printf("\n"); // adds new line after decrypted message
     ;
-    break;
+    break; //finishes case 5
 
-    case 6:
+    case 6: //runs Decipher substitution cipher #2 (key preset)
     
-    while (feof(input5)==0)
+    while (feof(input5)==0) //while input file has characters to be read (==0 is TRUE)
     {
-        fscanf(input5,"%c",&e);
+        fscanf(input5,"%c",&e); // calls input 4 and %c will read a single character for each loop
      
-        if (e == 'A') 
+        if (e == 'A') //the following script will substitute characters from input 4 for corresponding decryption characters found externally
         {
             e = 'K';
         }
@@ -308,19 +312,19 @@ switch (x)
                                                                                                 {
                                                                                                     e = 'L';
                                                                                                 }
-    			                                                                                printf("%c", e);
+    			                                                                                printf("%c", e); // prints one decrypted character each loop
     }
-    printf("\n");
+    printf("\n"); // adds new line after decrypted message
     ;
-    break;
+    break; //finishes case 6
 
-    case 7:
+    case 7: //Runs Encrypt using substitution cipher (key preset)
     
-    while (feof(input6)==0) 
+    while (feof(input6)==0) //while input file has characters to be read (==0 is TRUE)
     {
-        fscanf(input6,"%c",&f);
+        fscanf(input6,"%c",&f); // calls input 4 and %c will read a single character for each loop
      
-        if (f == 'A') 
+        if (f == 'A') //the following script will substitute characters from input 4 for corresponding decryption characters found externally
                 {
                     f = 'K';
                 }
@@ -412,14 +416,14 @@ switch (x)
                                                                                                         {
                                                                                                             f = 'L';
                                                                                                         }
-            			                                                                                printf("%c", f);
+            			                                                                                printf("%c", f); // prints one decrypted character each loop
     }
-    printf("\n");
+    printf("\n"); // adds new line after decrypted message
     ;
-    break;
+    break; //finishes case 7
     
-    default:
+    default: //Runs if a choice is not within given options
     
-    printf("please choose a valid option\n");
+    printf("please choose a valid option\n"); //for display purposes
 }
 }
